2541df425e3306b63541f05a54d0b183e315de1d
Yeongjin Jang <blue9057@gmail.com>
2016-03-24 04:43:50 -0400

    use https

--- post_to_s3.js
+++ post_to_s3.js
@@ -2,7 +2,7 @@ function post_data(filename, data) {
   // encrypt data with password
   // make it as base64
   // post!
-  var url = 'http://ec2-52-90-217-64.compute-1.amazonaws.com/upload.php';
+  var url = 'https://ec2-52-90-217-64.compute-1.amazonaws.com/upload.php';
   var posting = $.post(url, { 'filename' : filename,
          'data' : data });
   posting.done(function(data) {
